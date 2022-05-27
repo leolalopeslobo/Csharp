9a88f564cde9b34b55eee1d7fbc76a74b3cd6603
leolalopeslobo <83197830+leolalopeslobo@users.noreply.github.com>
2022-05-28 00:12:56 +0530

    printing text

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿//Displaying a formatted string
+﻿int a = 4;
+int b = 7;
 
-int x = 10;
-double y = 20.2;
-
-Console.WriteLine("x = {0}; y = {1}",x,y);
-Console.WriteLine("x = {0}; y = {1}",y,x);
-Console.WriteLine("x = {1}; y = {0}",x,y);
-Console.WriteLine("x = {1}; y = {0}",y,x);
+Console.Write(a);
+Console.Write(b);
