d04ac207a333f65b538d9ad2f7284f07f43ee8fb
leolalopeslobo <leolalopeslobo@gmail.com>
2022-05-27 23:19:02 +0530

    first c# program

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.Write("This is my first C# Program");
+Console.WriteLine("Today is the 27th of May 2022");
+Console.WriteLine("It's a Friday");
